6525c10890af910ea3c3c83db243b3838f9ade6f
Aaron Barack <atbarack@gmail.com>
2018-12-13 18:43:09 -0800

    Added LAT Interface

--- LAT.java
+++ LAT.java
@@ -1,6 +1,12 @@
-class LAT{  //main class for implementation  
-	protected class Vector(){}
-	protected class Matrix(){}
-	protected class LinearEquation(){}
+
+public interface LAT{  //main class for implementation  
+	protected class ParseInput(){} //String userInput
+	protected class Matrix(){} // Turns Vector into matrixs 
+	protected class LinearEquation(){
+		private String userInput; 
+		private Vector<float> Converter(); 
+		private Vector<float> DependanceRelation(Vector<Vector<float>> rrefMatrix); 
+	}
+	protected class Output(){} // Has print functions 
 
 }
