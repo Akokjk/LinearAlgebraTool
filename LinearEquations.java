824ed3466f7fec7a15eeaa6da0ce7c5c921457d0
Aaron Barack <atbarack@gmail.com>
2018-12-13 20:31:06 -0800

    Fucking fill these out and we are done

---
+++ LinearEquations.java
@@ -0,0 +1,379 @@
+import java.util.*; 
+public class LinearEquations{
+	private static String userInput;
+	private static String output; 
+	private static Vector<Vector<Float>> input; 
+	
+	
+	public static void parseInput() // takes the user input then converts it into the Vector<Vector<float>> input
+	{
+		
+	}
+	
+	public static void whatFunction() //figure out what function to run and print the results from the function 	
+	{
+		
+	}
+	
+	
+	
+	//40 functions go here return type will be output  then
+	
+	//Use Gaussian elimination to transform a matrix into a row equivalent matrix in (reduced) row echelon form.
+	private static void function_1(){
+		
+	}
+	
+	/*For a linear system L with augmented matrix A0 = (A|b) in echelon form
+	determine if L is inconsistent, has a unique solution, or has infinitely many
+	solutions.*/
+	private static void function_2(){
+		
+	}
+	/*For a homogeneous linear system L with coefficient matrix A in echelon
+	form determine if L has non-trivial solutions.
+	*/
+	private static void function_3(){
+		
+	}
+	/*For a consistent linear system L with augmented matrix A0 = (A|b) in
+	reduced echelon form determine the solution set of L.*/
+	private static void function_4(){
+		
+	}
+	/*For a homogeneous linear system L with coefficient matrix A in reduced
+	echelon form determine the solution set of L.*/
+	private static void function_5(){
+		
+	}
+	/*For a linear system L with augmented matrix A0 = (A|b) in reduced echelon
+	form find specific solutions to L.
+	*/
+	private static void function_6(){
+		
+	}
+	/* For a homogeneous linear system L with coefficient matrix A in reduced
+	echelon form find specific solutions to L.*/
+	private static void function_7(){
+		
+	}
+	/*For a sequence S = (v1, . . . , vn) of m−vectors and an m−vector u determine
+	if u is a linear combination of S (equivalently, if u ∈ Span(S).)*/
+	private static void function_8(){
+		
+	}
+	/*For a sequence S = (v1, . . . , vn) of m−vectors and an m−vector u ∈ Span(S)
+	find a sequence (c1, . . . , cn) of scalars such that u = c1v1 + · · · + cnvn.*/
+	private static void function_9(){
+		
+	}
+	/* For a sequence S = (v1, . . . , vn) of m−vectors determine if Span(S) = R
+	m.*/
+	private static void function_10(){
+		
+	}
+	/*Given a spanning sequence S = (v1, . . . , vl) for a subspace V of R
+	n find a
+	subsequence of B of S which is a basis of V .*/
+	private static void function_11(){
+		
+	}
+	/* Given a subspace V of R
+	n, a spanning sequence S = (v1, . . . , vl) of V and a
+	sequence B = (u1, . . . ,uk) of R
+	n determine if B is a basis of V .*/
+	private static void function_12(){
+		
+	}
+	/*Given a basis B = (v1, . . . , vk) of a subspace V of R
+	n and a vector u ∈ V
+	find the coordinate vector [u]B of u with respect to B.
+	*/
+	private static void function_13(){
+		
+	}
+	/*For an m × n matrix A and an m−vector b, determine if an n-vector x is a
+	solution to the matrix equation Ax = b.*/
+	private static void function_14(){
+		
+	}
+	/*Given an m × n matrix A and an n-vector x determine if x ∈ null(A).*/
+	private static void function_15(){
+		
+	}
+	/* Given an m × n matrix A and an m−vector b determine if b ∈ col(A).*/
+	private static void function_16(){
+		
+	}
+	/* Given an m × n matrix A and an m−vector b ∈ col(A) find an n-vector x
+	such that Ax = b.*/
+	private static void function_17(){
+		
+	}
+	/*Given an m × n matrix A find a basis for the null space of A.*/
+	private static void function_18(){
+		
+	}
+	/* For an m × n matrix A and an m−vector b ∈ col(A) express the solution
+	set to Ax = b in the form p + Span(B) where p is a particular solution and B
+	is a basis for null(A).*/
+	private static void function_19(){
+		
+	}
+	/*For an n × n matrix A determine if A is invertible.
+	*/
+	private static void function_20(){
+		
+	}
+	/*For an invertible n × n matrix A, compute A−1*/
+	private static void function_21(){
+		
+	}
+	/*For an n × n matrix A, use elementary row operations to compute det(A)*/
+	private static void function_22(){
+		
+	}
+	/*. Given det(A1), . . . , det(Ak) determine the determinant of A
+	e1
+	1
+	. . . Aek
+	k where
+	e1, . . . , ek are integers.*/
+	private static void function_23(){
+		
+	}
+	/* For a sequence S = (f1, . . . , fk) from Rn[x] and a vector g ∈ Rn[x]
+	determine if g ∈ Span(S).*/
+	private static void function_24(){
+		
+	}
+	/*For a sequence S = (f1, . . . , fk) from Rn[x] and a vector g ∈ Span(S) find
+	a sequence (c1, . . . , ck) of scalars such that g = c1f1 + · · · + ckfk.*/
+	private static void function_25(){
+		
+	}
+	/*For a sequence S = (m1, . . . ,mk) from Mm×n(R) and a vector n ∈
+	Mm×n(R) determine if m ∈ Span(S).*/
+	private static void function_26(){
+		
+	}
+	/*For a sequence S = (m1, . . . ,mk) from Mm×n(R) and a vector n ∈
+	Span(S). find a sequence (c1, . . . , ck) of scalars such that n = c1m1+· · ·+ckmk.
+	*/
+	private static void function_27(){
+		
+	}
+	/*For a sequence S = (f1, . . . , fk) from Rn[x] determine if Span(S) = Rn[x].*/
+	private static void function_28(){
+		
+	}
+	/*For a sequence S = (m1, . . . ,mk) from Mm×n(R) determine if Span(S) =
+	Mm×n(R).*/
+	private static void function_29(){
+		
+	}
+	/* For a sequence S = (f1, . . . , fk) from Rn[x] determine if S is linearly
+	independent or linearly dependent.
+	*/
+	private static void function_30(){
+		
+	}
+	/*For a linearly dependent sequence S = (f1, . . . , fk) from Rn[x] find a
+	non-trivial dependence relation (c1, . . . , ck).*/
+	private static void function_31(){
+		
+	}
+	/*For a sequence S = (m1, . . . ,mk) from Mm×n(R) determine if S is linearly
+	independent or linearly dependent.*/
+	private static void function_32(){
+		
+	}
+	/* For a linearly dependent sequence S = (m1, . . . ,mk) from Mm×n(R) find
+	a non-trivial dependence relation (c1, . . . , ck).*/
+	private static void function_33(){
+		
+	}
+	/*Determine if a sequence S = (f1, . . . , fk) from Rn[x] is a basis of Rn[x].*/
+	private static void function_34(){
+		
+	}
+	/*Determine if a sequence S = (m1, . . . ,mk) from Mm×n(R) is a basis of Mm×n(R). */
+	private static void function_35(){
+		
+	}
+	/*Let A be an m×n matrix with columns v1, . . . , vn. Determine dim(col(A)) = dim(Span(v1, . . . , vn)).*/
+	private static void function_36(){
+		
+	}
+	/* Given a sequence S = (f1, . . . , fk) of vectors from Rn[x] determine dim(Span(S))*/
+	private static void function_37(){
+		
+	}
+	/*Given a sequence S = (m1, . . . ,mk) from Mm×n(R) determine dim(Span(S)).*/
+	private static void function_38(){
+		
+	}
+	/*Given an m × n matrix A determine dim(null(A)).*/
+	private static void function_39(){
+		
+	}
+	/*For a basis B of Rn[x] and a vector g ∈ Rn[x] compute the coordinate vector of g with respect to B, [g]B.*/
+	private static void function_40(){
+		
+	}
+	/*. For a basis M of Mm×n(R) and a vector m ∈ Mm×n(R) compute the coordinate vector of m with respect to B, [m]B.*/
+	private static void function_41(){
+		
+	}
+	/*Given bases B and B 0 of a finite dimensional vector V compute the change of basis matrix from B to B 0.*/
+	private static void function_42(){
+		
+	}
+	/* Given a linear transformation T : V → W compute a basis of the range of T, R(T).*/
+	private static void function_43(){
+		
+	}
+	/* Given a linear transformation T : V → W compute Rank(T).*/
+	private static void function_44(){
+		
+	}
+	/* Given a linear transformation T : V → W determine if T is surjective (onto)*/
+	private static void function_45(){
+		
+	}
+	/*Given a linear transformation T : V → W compute a basis of the kernel of T, Ker(T).*/
+	private static void function_451(){
+		
+	}
+	/*Given a linear transformation T : V → W compute a Nullity(T). */
+	private static void function_46(){
+		
+	}
+	/*Given a linear transformation T : V → W determine if T is injective (one-to-one).*/
+	private static void function_47(){
+		
+	}
+	/*Given a linear transformation T : V → W determine if T is an isomorphism.*/
+	private static void function_48(){
+		
+	}
+	/* Given finite dimensional vector spaces V, W be with bases BV and BW
+	respectively and T : V → W a linear transformation, compute the matrix of T
+	with respect to (BV , BW ), MT (BV , BW ).
+	*/
+	private static void function_49(){
+		
+	}
+	/*Given finite dimensional vector spaces V, W be with bases BV and BW
+	respectively, T : V → W a linear transformation, the matrix MT (BV , BW ) of
+	T with respectt to (BV , BW ) and the coordinate vector [v]BV
+	of a vector in V compute the image, T(v).*/
+	private static void function_50(){
+		
+	}
+	/*Given finite dimensional vector spaces V, Wwith bases BV and BW respectively,
+	T : V → W a linear transformation, the matrix MT (BV , BW ) of T with
+	respect to (BV , BW ) and the coordinate vector [v]BV
+	of a vector in V determine
+	if v ∈ Ker(T).*/
+	private static void function_51(){
+		
+	}
+	/*Given finite dimensional vector spaces V, Wwith bases BV and BW respectively,
+	T : V → W a linear transformation, the matrix MT (BV , BW ) of T with
+	respect to (BV , BW ) find a basis for Ker(T).*/
+	private static void function_52(){
+		
+	}
+	/*Given finite dimensional vector spaces V, Wwith bases BV and BW respectively,
+	T : V → W a linear transformation, the matrix MT (BV , BW ) of T with
+	respect to (BV , BW ) compute Nullity(T).*/
+	private static void function_53(){
+		
+	}
+	/*Given finite dimensional vector spaces V, Wwith bases BV and BW respectively,
+	T : V → W a linear transformation, the matrix MT (BV , BW ) of T with
+	respect to (BV , BW ) determine if T is injective.*/
+	private static void function_54(){
+		
+	}
+	/*Given finite dimensional vector spaces V, W with bases BV and BW respectively,
+	T : V → W a linear transformation, the matrix MT (BV , BW ) of T
+	with respect to (BV , BW ) and the coordinate vector [w]BW of a vector in W
+	determine if w ∈ R(T)*/
+	private static void function_55(){
+		
+	}
+	/*Given finite dimensional vector spaces V, W with bases BV and BW respectively,
+	T : V → W a linear transformation, the matrix MT (BV , BW ) of T
+	with respect to (BV , BW ) and the coordinate vector [w]BW of a vector in W
+	determine if w ∈ R(T)*/
+	private static void function_56(){
+		
+	}
+	/*Given finite dimensional vector spaces V, W with bases BV and BW respectively,
+	T : V → W a linear transformation, and the matrix MT (BV , BW ) of
+	T with respect to (BV , BW ) find a basis for R(T).*/
+	private static void function_57(){
+		
+	}
+	/*Given finite dimensional vector spaces V, W with bases BV and BW respectively,
+	T : V → W a linear transformation, and the matrix MT (BV , BW ) of
+	T with respect to (BV , BW ) compute Rank(T).*/
+	private static void function_58(){
+		
+	}
+	/* Given finite dimensional vector spaces V, W with bases BV and BW respectively,
+	T : V → W a linear transformation, and the matrix MT (BV , BW ) of
+	T with respect to (BV , BW ) determine if T is surjective.*/
+	private static void function_59(){
+		
+	}
+	/*Given finite dimensional vector spaces V, W with bases BV and BW respectively,
+	T : V → W a linear transformation, and the matrix MT (BV , BW ) of T
+	with respect to (BV , BW ) determine if T is an isomorphism.*/
+	private static void function_60(){
+		
+	}
+	/* Given an n × n matrix A and a vector v ∈ R n determine if v is an eigenvector of A.*/
+	private static void function_61(){
+		
+	}
+	/* Given an n×n matrix A and an eigenvector v of A determine the eigenvalue which corresponds to v.*/
+	private static void function_62(){
+		
+	}
+	/*Given an n×n matrix A and a real number α determine if α is an eigenvalue of A.*/
+	private static void function_63(){
+		
+	}
+	/*Given an n × n matrix A and an eigenvalue α of A determine a basis for
+	the α-eigenspace of A, Eα(A).*/
+	private static void function_64(){
+		
+	}
+	/*Given an n × n matrix A compute the characteristic polynomial χA(λ).*/
+	private static void function_65(){
+		
+	}
+	/*Given an n × n matrix A compute the eigenvalues of A and determine
+	their algebraic and geometric multiplicity.*/
+	private static void function_66(){
+		
+	}
+	
+	public static void output()//show the user the awnser
+	{
+		
+	}
+	
+	
+	public static void main(String[] args){
+		while(!userInput.equals("exit")){
+			parseInput();  
+			whatFunction();
+			output(); 
+		}
+	}
+	
+}
